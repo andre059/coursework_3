0cd6a36ca4191f1d4da67da806b334c1895f35fa
andre059 <andreyperm159@mail.ru>
2023-03-01 16:49:24 +0500

    закончил

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from utils import filter_data, last_recent_values, format_data
 
 def mein():
     """
+<<<<<<< HEAD
     Получение json данных , завершение программы если нет данных, вывод данных для пользователя
+=======
+    Получение json данных и завершение программы если нет данных
+>>>>>>> 2396fa1 (закончил)
     :return:
     """
     with open('data_file.json', 'r', encoding="UTF-8") as file:
@@ -17,16 +21,24 @@ def mein():
             print('Информация получена успешно !\n')
 
         filtered_from = True
+<<<<<<< HEAD
         recent_values = 5
+=======
+        recent_values = 5  # последние значения
+>>>>>>> 2396fa1 (закончил)
 
         data = filter_data(data, filtered_from)
         data = last_recent_values(data, recent_values)
         data = format_data(data)
 
         print(f'Вывод данных:\n')
+<<<<<<< HEAD
 
         for row in data:
 
+=======
+        for row in data:
+>>>>>>> 2396fa1 (закончил)
             print(row, end='\n\n')
 
 
