f448af844a7db8239425324089933909ed4e00e2
andre059 <andreyperm159@mail.ru>
2023-03-01 20:51:47 +0500

    получение и вывод данных

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from utils import filter_data, last_recent_values, format_data
 
 def mein():
     """
-    Получение json данных и завершение программы если нет данных
+    Получение json данных , завершение программы если нет данных, вывод данных для пользователя
     :return:
     """
     with open('data_file.json', 'r', encoding="UTF-8") as file:
@@ -17,14 +17,16 @@ def mein():
             print('Информация получена успешно !\n')
 
         filtered_from = True
-        recent_values = 5  # последние значения
+        recent_values = 5
 
         data = filter_data(data, filtered_from)
         data = last_recent_values(data, recent_values)
         data = format_data(data)
 
         print(f'Вывод данных:\n')
+
         for row in data:
+
             print(row, end='\n\n')
 
 
